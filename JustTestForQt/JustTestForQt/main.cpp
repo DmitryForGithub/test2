3330963af94d6d0ace92cd4265547c0bd8b19130
DmitryForGithub <982067956@qq.com>
2020-05-13 11:54:07 +0800

    Signed-off-by: DmitryForGithub <982067956@qq.com>

--- JustTestForQt/JustTestForQt/main.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "JustTestForQt.h"
-#include <QtWidgets/QApplication>
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    JustTestForQt w;
-    w.show();
-    return a.exec();
-}
